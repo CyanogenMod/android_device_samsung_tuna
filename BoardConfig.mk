05af98f888922edaf809e3a3da027efb9f1105ec
Dmitry Shmidt <dimitrysh@google.com>
2011-07-25 15:13:12 -0700

    wifi: Add firmware path parameter for tethering and p2p
    Change-Id: Idfa528187265e889b24cbee46b17d9c35309943f
    Signed-off-by: Dmitry Shmidt <dimitrysh@google.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -65,6 +65,7 @@ BOARD_WPA_SUPPLICANT_PRIVATE_LIB := lib_driver_cmd_bcmdhd
 BOARD_HOSTAPD_DRIVER        := NL80211
 BOARD_HOSTAPD_PRIVATE_LIB   := lib_driver_cmd_bcmdhd
 BOARD_WLAN_DEVICE           := bcmdhd
+WIFI_DRIVER_FW_PATH_PARAM   := "/sys/module/bcmdhd/parameters/firmware_path"
 #WIFI_DRIVER_MODULE_PATH     := "/system/lib/modules/bcmdhd.ko"
 WIFI_DRIVER_FW_PATH_STA     := "/vendor/firmware/fw_bcmdhd.bin"
 WIFI_DRIVER_FW_PATH_P2P     := "/vendor/firmware/fw_bcmdhd_p2p.bin"
