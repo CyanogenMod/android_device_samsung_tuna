5e52233c8ed8a19a83f9fb33df9e7835c7942f81
Dima Zavin <dima@android.com>
2011-10-11 13:33:24 -0700

    tuna: create a symlink for hdcp_keys
    Change-Id: I3c42817cfcd581d211c29e756285cb7db0a14f7d
    Signed-off-by: Dima Zavin <dima@android.com>

--- Android.mk
+++ Android.mk
@@ -22,7 +22,25 @@
 
 LOCAL_PATH := $(call my-dir)
 
-# if some modules are built directly from this directory (not subdirectories),
-# their rules should be written here.
+include $(CLEAR_VARS)
+
+LOCAL_MODULE := tuna_hdcp_keys
+LOCAL_MODULE_TAGS := optional
+LOCAL_MODULE_CLASS := FAKE
+LOCAL_MODULE_SUFFIX := -timestamp
+
+include $(BUILD_SYSTEM)/base_rules.mk
+
+$(LOCAL_BUILT_MODULE): HDCP_KEYS_FILE := /factory/hdcp.keys
+$(LOCAL_BUILT_MODULE): SYMLINK := $(TARGET_OUT_VENDOR)/firmware/hdcp.keys
+$(LOCAL_BUILT_MODULE): $(LOCAL_PATH)/Android.mk
+$(LOCAL_BUILT_MODULE):
+	$(hide) echo "Symlink: $(SYMLINK) -> $(HDCP_KEYS_FILE)"
+	$(hide) mkdir -p $(dir $@)
+	$(hide) mkdir -p $(dir $(SYMLINK))
+	$(hide) rm -rf $@
+	$(hide) rm -rf $(SYMLINK)
+	$(hide) ln -sf $(HDCP_KEYS_FILE) $(SYMLINK)
+	$(hide) touch $@
 
 include $(call all-makefiles-under,$(LOCAL_PATH))
