3ab0af85e7c9407abebc74b10233d4c1f124160c
Andreas Huber <andih@google.com>
2012-02-29 16:41:46 -0800

    Codec components are now dynamically configured.
    Change-Id: Ife617162d1e1bb3e7102ffe1138d580a7b6f61ec

--- device.mk
+++ device.mk
@@ -59,6 +59,7 @@ PRODUCT_COPY_FILES := \
 	device/samsung/tuna/init.tuna.usb.rc:root/init.tuna.usb.rc \
 	device/samsung/tuna/ueventd.tuna.rc:root/ueventd.tuna.rc \
 	device/samsung/tuna/media_profiles.xml:system/etc/media_profiles.xml \
+	device/samsung/tuna/media_codecs.xml:system/etc/media_codecs.xml \
 	device/samsung/tuna/gps.conf:system/etc/gps.conf
 
 # Bluetooth configuration files
