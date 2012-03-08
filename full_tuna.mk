2e1364efe12bd5eeac9cc97b6369f3ef8b5c1ce9
Chih-Chung Chang <chihchung@google.com>
2012-03-08 20:09:51 +0800

    Remove Camera package (it's merged into Gallery package).
    Change-Id: I74762925d0bc0fee114627223e157091b0c0463f

--- full_tuna.mk
+++ full_tuna.mk
@@ -21,10 +21,6 @@
 # lines, full and toro, hence its name.
 #
 
-# Camera
-PRODUCT_PACKAGES := \
-    Camera
-
 # Inherit from those products. Most specific first.
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base.mk)
 # This is where we'd set a backup provider if we had one
