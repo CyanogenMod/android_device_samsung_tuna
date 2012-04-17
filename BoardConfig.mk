e7570183f3581ee7407905da0b45d894d2a8a8ad
Sean Donovan <swordrune10@gmail.com>
2012-04-16 20:59:35 -0500

    Support inline kernel building
    Patch 5: change kernel config to match repo
    
    Change-Id: Ia04157d2e3cb495e20a76775b4d3ef6596f3191a
    Signed-off-by: Sean Donovan <swordrune10@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -32,6 +32,10 @@ TARGET_NO_BOOTLOADER := true
 BOARD_KERNEL_BASE := 0x80000000
 # BOARD_KERNEL_CMDLINE :=
 
+# Define kernel config for inline building
+TARGET_KERNEL_CONFIG := tuna_defconfig
+TARGET_KERNEL_SOURCE := kernel/samsung/tuna
+
 TARGET_PREBUILT_KERNEL := device/samsung/tuna/kernel
 
 TARGET_NO_RADIOIMAGE := true
--- device.mk
+++ device.mk
@@ -19,12 +19,6 @@
 #
 # Everything in this directory will become public
 
-ifeq ($(TARGET_PREBUILT_KERNEL),)
-LOCAL_KERNEL := device/samsung/tuna/kernel
-else
-LOCAL_KERNEL := $(TARGET_PREBUILT_KERNEL)
-endif
-
 DEVICE_PACKAGE_OVERLAYS := device/samsung/tuna/overlay
 
 # This device is xhdpi.  However the platform doesn't
@@ -55,7 +49,6 @@ PRODUCT_PACKAGES += \
 	tuna_hdcp_keys
 
 PRODUCT_COPY_FILES := \
-	$(LOCAL_KERNEL):kernel \
 	device/samsung/tuna/init.tuna.rc:root/init.tuna.rc \
 	device/samsung/tuna/init.tuna.usb.rc:root/init.tuna.usb.rc \
 	device/samsung/tuna/ueventd.tuna.rc:root/ueventd.tuna.rc \
