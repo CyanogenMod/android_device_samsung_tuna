8d39a6ec99f7991baa983f6bb398af5d2e1f4d68
David Thompson <david.w.thompson.321@gmail.com>
2012-05-28 22:24:28 +0100

    tuna: Automatically mount USB storage and show unmount button and storage usage in settings
    mount point is /mnt/usbdisk
    
    Change-Id: I636aa2f3f45be3d3ed2a120c90d67b13b467c2c9

--- device.mk
+++ device.mk
@@ -190,3 +190,7 @@ $(call inherit-product-if-exists, vendor/samsung/tuna/device-vendor.mk)
 BOARD_WLAN_DEVICE_REV := bcm4330_b2
 WIFI_BAND             := 802_11_ABG
 $(call inherit-product-if-exists, hardware/broadcom/wlan/bcmdhd/firmware/bcm4330/device-bcm.mk)
+
+# vold
+PRODUCT_COPY_FILES += \
+	device/samsung/tuna/vold.fstab:/system/etc/vold.fstab
