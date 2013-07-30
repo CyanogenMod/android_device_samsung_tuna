5d5fed712239c2e15360c814b34da830797dfcd2
Helvio Pedreschi <helvio88@gmail.com>
2013-07-30 17:35:55 -0300

    Enable Bluetooth 4.0 (BLE) for tuna-based devices
     - Needed to:
         http://review.cyanogenmod.org/46543
         http://review.cyanogenmod.org/46989
    
    Change-Id: If299d291a401ddeff17b6d0db5b2df1059ea3a18

--- device.mk
+++ device.mk
@@ -122,6 +122,7 @@ PRODUCT_COPY_FILES += \
 # These are the hardware-specific features
 PRODUCT_COPY_FILES += \
 	frameworks/native/data/etc/handheld_core_hardware.xml:system/etc/permissions/handheld_core_hardware.xml \
+	frameworks/native/data/etc/android.hardware.bluetooth_le.xml:system/etc/permissions/android.hardware.bluetooth_le.xml \
 	frameworks/native/data/etc/android.hardware.camera.flash-autofocus.xml:system/etc/permissions/android.hardware.camera.flash-autofocus.xml \
 	frameworks/native/data/etc/android.hardware.camera.front.xml:system/etc/permissions/android.hardware.camera.front.xml \
 	frameworks/native/data/etc/android.hardware.location.gps.xml:system/etc/permissions/android.hardware.location.gps.xml \
