8557e160fe858dba1dc5443b8d51b025abe3070f
Michael Limb <something15525@gmail.com>
2013-11-07 09:25:32 -0800

    tuna: update sensors to new layout
    65xx has been added for hammerhead, need to update to reflect 60xx
    for tuna (manta needs this as well)

--- libsensors/Android.mk
+++ libsensors/Android.mk
@@ -26,7 +26,7 @@ LOCAL_MODULE_PATH := $(TARGET_OUT_SHARED_LIBRARIES)/hw
 LOCAL_MODULE_TAGS := optional
 
 LOCAL_CFLAGS := -DLOG_TAG=\"Sensors\"
-LOCAL_C_INCLUDES += hardware/invensense/libsensors
+LOCAL_C_INCLUDES += hardware/invensense/60xx/libsensors
 LOCAL_SRC_FILES := \
 	sensors.cpp \
 	InputEventReader.cpp \
