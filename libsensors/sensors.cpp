75364b5eb78f425c6cd2d06765318a021dc9b721
Kyle Repinski <repinski23@gmail.com>
2014-07-04 15:51:51 -0500

    tuna: Fix sensors for KitKat+ in a more appropriate manner.
    Change-Id: I13f3d62a4022b2c6796c101dc9a5024966485159
    Signed-off-by: Kyle Repinski <repinski23@gmail.com>

--- libsensors/sensors.cpp
+++ libsensors/sensors.cpp
@@ -80,15 +80,27 @@ static struct sensor_t sSensorList[LOCAL_SENSORS + MPLSensor::numSensors] = {
       { "GP2A Light sensor",
           "Sharp",
           1, SENSORS_LIGHT_HANDLE,
-          SENSOR_TYPE_LIGHT, powf(10, 125.0f/ 24.0f) * 4, 1.0f, 0.75f, 0, 0, 0, { } },
+          SENSOR_TYPE_LIGHT, powf(10, 125.0f/ 24.0f) * 4, 1.0f, 0.75f, 0,
+#ifdef SENSORS_DEVICE_API_VERSION_1_1
+          0, 0,
+#endif
+          { } },
       { "GP2A Proximity sensor",
           "Sharp",
           1, SENSORS_PROXIMITY_HANDLE,
-          SENSOR_TYPE_PROXIMITY, 5.0f, 5.0f, 0.75f, 0, 0, 0, { } },
+          SENSOR_TYPE_PROXIMITY, 5.0f, 5.0f, 0.75f, 0,
+#ifdef SENSORS_DEVICE_API_VERSION_1_1
+          0, 0,
+#endif
+          { } },
       { "BMP180 Pressure sensor",
           "Bosch",
           1, SENSORS_PRESSURE_HANDLE,
-          SENSOR_TYPE_PRESSURE, 1100.0f, 0.01f, 0.67f, 20000, 0, 0, { } },
+          SENSOR_TYPE_PRESSURE, 1100.0f, 0.01f, 0.67f, 20000,
+#ifdef SENSORS_DEVICE_API_VERSION_1_1
+          0, 0,
+#endif
+          { } },
 };
 static int numSensors = LOCAL_SENSORS;
 
