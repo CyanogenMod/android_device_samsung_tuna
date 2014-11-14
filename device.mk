fea392cd5bafb7d9fd884b2d6cc85e54f208a497
Ziyann <jaraidaniel@gmail.com>
2014-11-14 11:36:50 +0100

    tuna: move nfc files to nfc/
    Change-Id: I9a1f5cf85538c6b69fc3cc54a09263b7e910c228

--- device.mk
+++ device.mk
@@ -165,9 +165,9 @@ PRODUCT_COPY_FILES += \
 
 # NFCEE access control
 ifeq ($(TARGET_BUILD_VARIANT),user)
-    NFCEE_ACCESS_PATH := device/samsung/tuna/nfcee_access.xml
+    NFCEE_ACCESS_PATH := device/samsung/tuna/nfc/nfcee_access.xml
 else
-    NFCEE_ACCESS_PATH := device/samsung/tuna/nfcee_access_debug.xml
+    NFCEE_ACCESS_PATH := device/samsung/tuna/nfc/nfcee_access_debug.xml
 endif
 PRODUCT_COPY_FILES += \
     $(NFCEE_ACCESS_PATH):system/etc/nfcee_access.xml
