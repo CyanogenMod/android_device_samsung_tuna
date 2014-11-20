a9b1b607e6fb99de9720773ff16619f8409d5e89
Ziyann <jaraidaniel@gmail.com>
2014-11-20 11:09:25 +0100

    tuna: fix recovery build
    Change-Id: I3577715fcb30004747ef184eeb41bc6d0acb389d

--- recovery/Android.mk
+++ recovery/Android.mk
@@ -17,7 +17,7 @@ include $(CLEAR_VARS)
 # Edify extension functions for doing bootloader updates on Tuna devices.
 
 LOCAL_MODULE_TAGS := optional
-LOCAL_C_INCLUDES += bootable/recovery system/vold
+LOCAL_C_INCLUDES += bootable/recovery system/vold external/openssl/include
 LOCAL_SRC_FILES := recovery_updater.c bootloader.c
 
 # should match TARGET_RECOVERY_UPDATER_LIBS set in BoardConfig.mk
