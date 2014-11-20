b578d44123096aa41a6e6d328d416fdcc9ac5d01
Ziyann <jaraidaniel@gmail.com>
2014-11-20 11:44:00 +0100

    tuna: stagefright: use media_codecs_google*.xml files
    Change-Id: I66d6128fa2e88b8986dc88625bf7ff08de7f7049

--- device.mk
+++ device.mk
@@ -88,6 +88,9 @@ PRODUCT_COPY_FILES += \
 
 # Media profiles
 PRODUCT_COPY_FILES += \
+	frameworks/av/media/libstagefright/data/media_codecs_google_audio.xml:system/etc/media_codecs_google_audio.xml \
+	frameworks/av/media/libstagefright/data/media_codecs_google_telephony.xml:system/etc/media_codecs_google_telephony.xml \
+	frameworks/av/media/libstagefright/data/media_codecs_google_video.xml:system/etc/media_codecs_google_video.xml \
 	$(DEVICE_FOLDER)/media_profiles.xml:system/etc/media_profiles.xml \
 	$(DEVICE_FOLDER)/media_codecs.xml:system/etc/media_codecs.xml
 
