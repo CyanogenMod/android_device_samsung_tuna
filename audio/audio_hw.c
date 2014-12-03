e4e25a6a70f3ae277b28df777f6c3e16adfeba6d
Ziyan <jaraidaniel@gmail.com>
2014-12-03 12:53:48 +0100

    tuna: audio: Add audio_input_flags_t to HAL open_input_stream

--- audio/audio_hw.c
+++ audio/audio_hw.c
@@ -3492,7 +3492,8 @@ static int adev_open_input_stream(struct audio_hw_device *dev,
                                   audio_io_handle_t handle,
                                   audio_devices_t devices,
                                   struct audio_config *config,
-                                  struct audio_stream_in **stream_in)
+                                  struct audio_stream_in **stream_in,
+                                  audio_input_flags_t flags __unused)
 {
     struct tuna_audio_device *ladev = (struct tuna_audio_device *)dev;
     struct tuna_stream_in *in;
