70e8e14531bfb31b6b8da644d99f16e75db50a28
Ziyan <jaraidaniel@gmail.com>
2014-12-31 13:09:03 +0100

    tuna: remove custom recovery ui
    The default UI can do the same now.
    
    Change-Id: I20946210f5dff40f65490fc81e98b087795b440b

--- recovery/Android.mk
+++ recovery/Android.mk
@@ -3,17 +3,6 @@ ifneq (,$(findstring $(TARGET_DEVICE),tuna toro toroplus maguro))
 LOCAL_PATH := $(call my-dir)
 include $(CLEAR_VARS)
 
-LOCAL_MODULE_TAGS := eng
-LOCAL_C_INCLUDES += bootable/recovery
-LOCAL_SRC_FILES := recovery_ui.cpp
-
-# should match TARGET_RECOVERY_UI_LIB set in BoardConfig.mk
-LOCAL_MODULE := librecovery_ui_tuna
-
-include $(BUILD_STATIC_LIBRARY)
-
-include $(CLEAR_VARS)
-
 # Edify extension functions for doing bootloader updates on Tuna devices.
 
 LOCAL_MODULE_TAGS := optional
