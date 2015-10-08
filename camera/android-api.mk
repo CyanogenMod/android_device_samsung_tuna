731f282f33a6f30fc0a2378136100f8cab7c1825
Ziyan <jaraidaniel@gmail.com>
2015-10-09 01:53:59 +0200

    android-api: add MM
    Change-Id: I0a17b1fd55aa1488e7b8c4cdde4d8aebc02bc6b5

--- camera/android-api.mk
+++ camera/android-api.mk
@@ -1,5 +1,6 @@
 
 # Makefile variables and C/C++ macros to recognize API level
+ANDROID_API_MM_OR_LATER :=
 ANDROID_API_LP_OR_LATER :=
 ANDROID_API_KK_OR_LATER :=
 ANDROID_API_JB_MR1_OR_LATER :=
@@ -7,6 +8,10 @@ ANDROID_API_JB_OR_LATER :=
 ANDROID_API_ICS_OR_LATER :=
 ANDROID_API_CFLAGS :=
 
+ifeq ($(shell test $(PLATFORM_SDK_VERSION) -ge 23 || echo 1),)
+    ANDROID_API_MM_OR_LATER := true
+    ANDROID_API_CFLAGS += -DANDROID_API_MM_OR_LATER
+endif
 ifeq ($(shell test $(PLATFORM_SDK_VERSION) -ge 20 || echo 1),)
     ANDROID_API_LP_OR_LATER := true
     ANDROID_API_CFLAGS += -DANDROID_API_LP_OR_LATER
@@ -29,6 +34,7 @@ ifeq ($(shell test $(PLATFORM_SDK_VERSION) -ge 14 || echo 1),)
 endif
 
 define clear-android-api-vars
+$(eval ANDROID_API_MM_OR_LATER:=) \
 $(eval ANDROID_API_LP_OR_LATER:=) \
 $(eval ANDROID_API_KK_OR_LATER:=) \
 $(eval ANDROID_API_JB_MR1_OR_LATER:=) \
